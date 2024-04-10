f954ff776eab7ec794b5ef44eca559d33cf64f0a
Pragun Bhutani <pragun.bhutani897@gmail.com>
2024-04-10 20:40:25 +0530

    stuff

---
+++ FETCH-API/index.js
@@ -0,0 +1,38 @@
+const URL = "https://cat-fact.herokuapp.com/facts/";
+const factPara = document.querySelector("#fact");
+const btn = document.querySelector("#btn");
+// IN FETCH API RETURN PROMISE
+// *** get request  ***
+// let pr = fetch(URL);
+// console.log(pr);
+
+const getFacts = async () => {
+  //   console.log("getting data....");
+  let response = await fetch(URL);
+  //   console.log(response); // json format
+  //   console.log(response.status);
+  //   console.log(response.body);
+  let data = await response.json(); // json to js object  .. parser data to js object from json
+  //   console.log(data);
+  //   console.log(data[0].text);
+  factPara.innerText = data[0].text;
+};
+
+// // BY promise chaining
+// function getFacts() {
+//   fetch(URL)
+//     .then((response) => {
+//       return response.json();
+//     })
+//     .then((data) => {
+//       console.log(data);
+//       factPara.innerText = data[0].text;
+//     });
+// }
+
+btn.addEventListener("click", getFacts);
+
+// getFacts;
+// getFacts();
+
+// header in api contain additional info
